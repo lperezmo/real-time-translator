1a35c75f56cb1a29bdee69da163b129e7e3edd75
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2023-08-02 01:17:45 -0500

    Update app.py
    Added check to prevent recordings over 10 minutes.

--- app.py
+++ app.py
@@ -37,7 +37,12 @@ def main():
 			with cols[0]:
 				st.markdown("***Original Audio***")
 				st.audio(audio_bytes, format="audio/wav")
-				st.session_state.audio_bytes = audio_bytes\
+				# Check if audio is longer than 10 minutes
+				if len(audio_bytes) > 48000000:
+					st.warning('Please keep your audio recordings under 10 minutes, thanks!')
+					st.stop()
+				else:
+					st.session_state.audio_bytes = audio_bytes\
 
 	if 'audio_bytes' in st.session_state:
 		# st.info('Audio successfully recorded, translating...')
