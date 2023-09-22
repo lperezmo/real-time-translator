aee0b024c34698186507e087c3f3ba9e395ee0ee
Luis Perez Morales <lperezmo@uoregon.edu>
2023-09-22 06:15:59 -0500

    Removed unecessary files and refactored code to make it shorter and simpler

--- app.py
+++ app.py
@@ -4,124 +4,75 @@ from gtts import gTTS
 import streamlit as st
 from io import BytesIO
 from audio_recorder_streamlit import audio_recorder
-import streamlit.components.v1 as components
-from st_custom_components import st_audiorec
 
-#---------------------------------#
 # Set page configuration
-#---------------------------------#
-st.set_page_config(page_title='Real Time Translation', 
-					page_icon='🌎', 
-					layout='centered', 
-					initial_sidebar_state='auto')
+st.set_page_config(
+    page_title='Real Time Translation',
+    page_icon='🌎',
+    layout='centered',
+    initial_sidebar_state='auto'
+)
+
+# Hide Streamlit footer
 hide_streamlit_style = """
-			<style>
-			footer {visibility: hidden;}
-			</style>
-			"""
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+<style>
+    footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-#---------------------------------#
-# Main
-#---------------------------------#
+
 def main():
-	st.header('Real Time Translation')
-	st.caption('Written by LPM')
+    st.header('Real Time Translation')
+    st.caption('Written by LPM')
 
-	# OpenAI API key
-	openai.api_key = st.secrets["OPENAI_API_KEY"]
+    # Set OpenAI API key
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-	audio_bytes = audio_recorder(pause_threshold = 40)
-	if audio_bytes:
-		# Check if audio is less than half a second
-		if len(audio_bytes) > 8000:
-				st.success('Audio captured correctly')
-		else:
-			st.warning('Audio captured incorrectly, please try again.')
-		st.audio(audio_bytes, format="audio/wav")
-		st.session_state.audio_bytes = audio_bytes\
-		
-	# Form for real time translation
-	with st.form('input_form'):
-		# st.subheader('Real Time Speech Translation')
+    audio_bytes = audio_recorder(pause_threshold=40)
+    if audio_bytes:
+        # Check if audio is of sufficient length
+        if len(audio_bytes) > 8000:
+            st.success('Audio captured correctly')
+        else:
+            st.warning('Audio captured incorrectly, please try again.')
+        st.audio(audio_bytes, format="audio/wav")
+        st.session_state.audio_bytes = audio_bytes
 
-		# Submit button
-		submit_button = st.form_submit_button(label='Translate', type='primary')
-		if submit_button:
-			if 'audio_bytes' in st.session_state:
-				if len(st.session_state.audio_bytes) > 0:
-					# Translate audio bytes into English
-					audio_file = io.BytesIO(st.session_state.audio_bytes)
-					audio_file.name = "temp_audio_file.wav"
-					transcript = openai.Audio.translate("whisper-1", audio_file)
-					# st.success('Translation successful!')
-					st.markdown("***Translation Transcript***")
-					st.text_area('transcription', transcript['text'], label_visibility = 'collapsed')
-					if len(transcript['text']) > 0: 
-						# Convert text to speech
-						sound_file = BytesIO()
-						tts = gTTS(transcript['text'], lang='en')
-						tts.write_to_fp(sound_file)
-						st.markdown("***Synthesized Speech Translation***")
-						st.audio(sound_file)
-					else:
-						st.warning('No text to convert to speech.')
-				else:
-					st.warning('No audio recorded, please make sure your audio got recorded correctly.')
-			else:
-				st.warning('No audio recorded, please make sure your audio got recorded correctly.')
-			
-	# Text to speech
-	with st.expander("Text to speech"):
-		with st.form('text_to_speech'):
-			# st.subheader('Text to Speech')
-			text_to_speech = st.text_area('Enter text to convert to speech')
-			submit_button = st.form_submit_button(label='Convert')
-			if submit_button and len(text_to_speech) > 0:
-				# Convert text to speech
-				sound_file = BytesIO()
-				tts = gTTS(text_to_speech, lang='en')
-				tts.write_to_fp(sound_file)
-				st.audio(sound_file)
+    # Form for real-time translation
+    with st.form('input_form'):
+        submit_button = st.form_submit_button(label='Translate', type='primary')
+        if submit_button and 'audio_bytes' in st.session_state and len(st.session_state.audio_bytes) > 0:
+            # Translate audio bytes into English
+            audio_file = io.BytesIO(st.session_state.audio_bytes)
+            audio_file.name = "temp_audio_file.wav"
+            transcript = openai.Audio.translate("whisper-1", audio_file)
+            st.markdown("***Translation Transcript***")
+            st.text_area('transcription', transcript['text'], label_visibility='collapsed')
+            if transcript['text']:
+                # Convert text to speech
+                sound_file = BytesIO()
+                tts = gTTS(transcript['text'], lang='en')
+                tts.write_to_fp(sound_file)
+                st.markdown("***Synthesized Speech Translation***")
+                st.audio(sound_file)
+            else:
+                st.warning('No text to convert to speech.')
+        else:
+            st.warning('No audio recorded, please ensure your audio was recorded correctly.')
 
-def check_password():
-	"""
-	A function that return True if the password is correct, False otherwise.
-	"""
+    # Text to speech section
+    with st.expander("Text to speech"):
+        with st.form('text_to_speech'):
+            text_to_speech = st.text_area('Enter text to convert to speech')
+            submit_button = st.form_submit_button(label='Convert')
+            if submit_button and text_to_speech:
+                # Convert text to speech
+                sound_file = BytesIO()
+                tts = gTTS(text_to_speech, lang='en')
+                tts.write_to_fp(sound_file)
+                st.audio(sound_file)
 
-	def password_entered():
-		"""
-		Checks whether a password entered by the user is correct.
-		"""
-		if st.session_state["password"] == st.secrets["user_pass"]:
-			st.session_state["password_correct"] = True
-			del st.session_state["password"]  # don't store password
-		else:
-			st.session_state["password_correct"] = False
 
-	if "password_correct" not in st.session_state:
-		# First run, show input for password.
-		st.text_input(
-			"Password", type="password", on_change=password_entered, key="password"
-		)
-		return False
-	
-	elif not st.session_state["password_correct"]:
-		# Password not correct, show input + error.
-		st.text_input(
-			"Password", type="password", on_change=password_entered, key="password"
-		)
-		st.error("Password incorrect")
-		return False
-	else:
-		# Password correct.
-		return True
-	
-#---------------------------------#
-# Run the main function
-#---------------------------------#
 if __name__ == '__main__':
-	#if st.button("Translate recorded audio", type="primary"):
-	#	st.experimental_rerun()
-	# if check_password():
-	main()
+    main()
